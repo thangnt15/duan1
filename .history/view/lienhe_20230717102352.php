543e49c210695bb3bd7017a6709b881bc825fa54
thangnt15 <imlight2001@gmail.com>
2023-07-17 10:41:30 +0700

    include giới thiệu, liên hệ

---
+++ .history/view/lienhe_20230717102352.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liên hệ</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+    <link rel="stylesheet" href="css/lienHe.css">
+    <link href="//fonts.googleapis.com/css?family=Poppins:400,200,300,400,400i,500,600,600i,700,700i,800,900,900i" rel="stylesheet" type="text/css" media="all">
+</head>
+
+<body>
+    <main>
+        <div class="lienHe">
+            <h1>Liên hệ</h1>
+            <div class="content_lienHe">
+                <div class="content">
+                    <div class="icon">
+                        <i class="fas fa-phone"></i>
+                    </div>
+                    <div class="text">
+                        <h4>Dịch vụ khách hàng:</h4>
+                        <p>0345-561-321</p>
+                    </div>
+                </div>
+                <!-- end content -->
+                <div class="content">
+                    <div class="icon">
+                        <i class="fas fa-map-marker-alt"></i>
+                    </div>
+                    <div class="text">
+                        <h4>Địa chỉ:</h4>
+                        <p>Số 141, Chiến Thắng, Xã Tân Triều, Huyện Thanh Trì, Hà Nội</p>
+                    </div>
+                </div>
+                <!-- end content -->
+                <div class="content">
+                    <div class="icon">
+                        <i class="fas fa-headset"></i>
+                    </div>
+                    <div class="text">
+                        <h4>Email:</h4>
+                        <p>penguin@gmail.com</p>
+                    </div>
+                </div>
+                <!-- end content -->
+            </div>
+        </div>
+        <!-- end lienHe -->
+
+        <div class="googleMap">
+            <iframe class="map" src="https://map.coccoc.com/map/1392703755587958?borders=20.96940416734322%2C105.75945644245611%2C20.99645108371014%2C105.82348613605963&query=h%E1%BB%8Dc+vi%E1%BB%87n+k%E1%BB%B9+thu%E1%BA%ADt+m%E1%BA%ADt+m%C3%A3&bounds_change=true" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+        <!-- end googleMap -->
+    </main>
+</body>
+
+</html>
