5e0dd5cc55d3a201bec24eb6b9a13252b23651be
quanpaph26348 <127657206+quanpaph26348@users.noreply.github.com>
2023-08-09 14:34:05 +0700

    update thongke

--- model/thongke.php
+++ model/thongke.php
@@ -3,7 +3,7 @@
 function  loadall_thongke() {
     $sql = " select danhmuc.id as madm,
      danhmuc.name as tendm ,count(sanpham.id) 
-     as countsp, min(sanpham.price) as minprice, max(sanpham.price) as maxprice,avg(sanpham.price) as avgprice";
+     as countsp, min(sanpham.giamoi) as minprice, max(sanpham.giamoi) as maxprice,avg(sanpham.giamoi) as avgprice";
     $sql.=" from sanpham  left join danhmuc on danhmuc.id =sanpham.iddm where 1 ";
     $sql.=" group by danhmuc.id order by danhmuc.id desc ";
     $listtk = pdo_query($sql);
